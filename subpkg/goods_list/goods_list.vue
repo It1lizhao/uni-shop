857c6419a9301b2f01e92519a74dd8c57c6f2201
LZ <2739542493@qq.com>
2023-03-05 18:22:07 +0800

    完成了商品列表功能的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,7 +1,11 @@
 <template>
   <view>
-
-  </view>
+    <view class="goods-list">
+      <view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+        <!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+        <my-goods :goods="goods"></my-goods>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -9,10 +13,75 @@
   export default {
     data() {
       return {
+        // 请求参数对象
+        queryObj: {
+          // 查询关键词
+          query: '',
+          // 商品分类Id
+          cid: '',
+          // 页码值
+          pagenum: 1,
+          // 每页显示多少条数据
+          pagesize: 10
+        },
+        // 商品列表的数据
+        goodsList: [],
+        // 总数量，用来实现分页
+        total: 0,
+        islodaing: false //节流
+      }
+    },
+    onLoad(options) {
+      // 将页面参数转存到 this.queryObj 对象中
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
 
-      };
+      // 调用获取商品列表数据的方法
+      this.getGoodsList()
+    },
+    methods: {
+      // 获取商品列表数据的方法
+      async getGoodsList(callback) {
+        this.islodaing = true
+        // 发起请求
+        const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        this.islodaing = false
+        // 只要数据请求完毕，就立即按需调用 cb 回调函数
+        callback && callback()
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 为数据赋值
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      },
+
+      // 点击跳转商品详情
+      gotoDetail(goods) {
+        uni.navigateTo({
+          url: `/subpkg/goods_detail/goods_detail?goods_id=${goods.goods_id}`
+        })
+      }
+    },
+    // 触底的事件
+    onReachBottom() {
+      // 判断是否还有下一页数据
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+      // 判断是否正在请求其它数据，如果是，则不发起额外的请求
+      if (this.islodaing) return
+      this.queryObj.pagenum += 1
+      this.getGoodsList()
     },
 
+    // 下拉刷新
+    onPullDownRefresh() {
+      // 清空一些信息
+      this.queryObj.pagenum = 1
+      this.goodsList = []
+      this.total = 0
+      this.islodaing = false
+
+      // 重新发起请求,传一个关闭下拉刷新的回调函数
+      this.getGoodsList(() => uni.stopPullDownRefresh())
+    }
   }
 </script>
 
